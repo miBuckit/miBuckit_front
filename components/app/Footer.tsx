4836540ee216c44037a43681b4571bf7a1c47543
DESKTOP-B6OB3TR\USER <jaksimfive@naver.com>
2023-06-28 00:19:40 +0900

    footer_done

--- components/app/Footer.tsx
+++ components/app/Footer.tsx
@@ -10,56 +10,30 @@ const navigation = [
 function classNames(...classNamees: string[]) {
   return classNamees.filter(Boolean).join(" ");
 }
-
-export default function Navbar() { 
+//id나 class로 빼서 상위에서 css한꺼번에 설정하기
+export default function Footer() { 
   return (
-    <div>
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button type="button" className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-              <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-              </svg>
-              <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" /> 
-              </svg>
-            </button>
+    <div className="absolute bottom-0 w-screen bg-yellow-200 py-20">
+      <div className="mx-auto max-w-7xl px-20">
+        <div className="">
+          <div className="basis-1/4">
+            <ul className="flex flex-wrap flex-row items-stretch justify-between">
+            <li className="basis-1/4 mb-4"><a>이용 약관</a></li>
+            <li className="basis-1/4 mb-4"><a>회사 정보</a></li>
+            <li className="basis-1/4 mb-4"><a>고객 센터</a></li>
+            <li className="basis-1/4 mb-4"><a>입사 정보</a></li>
+            <li className="basis-1/4 mb-4"><a>문의하기</a></li>
+            <li className="basis-1/4 mb-4"><a>개인정보</a></li>
+            <li className="basis-1/4 mb-4"><a>법적고지</a></li>
+            <li className="basis-1/4 mb-4"><a>쿠키설정</a></li>
+            </ul>
           </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <img className="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"></img>
-              <img className="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"></img>
-            </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">메인</a>
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">상세</a>
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">프로필</a>
-              </div>
-            </div>
-          </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button type="button" className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-              <span className="sr-only">View notifications</span>
-              <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-              </svg>
-            </button>
-            <div className="relative ml-3">
-              <div>
-                <button type="button" className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                  <span className="sr-only">Open user menu</span>
-                  <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""></img>
-                </button>
-              </div>
-              <div className="absolute hidden right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-0">Your Profile</a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-1">Settings</a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-2">Sign out</a>
-              </div>
-            </div>
+        </div>
+      </div>
+      <div className="mx-auto max-w-7xl px-20">
+        <div className="">
+          <div className="">
+           <p className="mt-6 text-sm">미버킷코리아<br/>대표 : 오은혜만<br/>주소 : 대한민국 서울특별시 마포구 상암<br/>사업자등록번호 : 941022</p>
           </div>
         </div>
       </div>
